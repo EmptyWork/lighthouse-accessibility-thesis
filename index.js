91894c3637e0f1776afea38ecbd6809ceed74e46
emptywork <22065214+EmptyWork@users.noreply.github.com>
2023-06-10 10:58:31 +0900

    Update `index.js` added fully customize option for categories

--- index.js
+++ index.js
@@ -2,16 +2,34 @@ import { series } from 'async'
 import { exec } from 'child_process'
 import { readFileSync, writeFileSync, existsSync, mkdirSync, copyFileSync, write, writeFile } from 'fs'
 if (!existsSync('./src/urlList.js')) copyFileSync('./src/urlList.example', './src/urlList.js')
-import { urlList } from './src/urlList.js'
+import { urlList, options } from './src/urlList.js'
+
+Object.prototype.isEmpty = (obj) => {
+    for (const prop in obj) {
+        if (Object.hasOwn(obj, prop)) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 const execResult = (err = null, out, outerr = null) => {
+
     let accessibilityScores = (!existsSync('./out/scores.json')) ? readFileSync('./src/scores.json') : readFileSync('./out/scores.json')
 
     const data = JSON.parse(out)
     const accessibilityScoresJSON = JSON.parse(accessibilityScores)
-    const URLaccessibilityScoreResult = data?.categories?.accessibility?.score
+    const categoriesScoresObject = {}
+    const optionCategories = options.categories ?? ["accessibility", "pwa", "best-practices", "performance", "seo"];
 
-    accessibilityScoresJSON[data.requestedUrl] = URLaccessibilityScoreResult
+    optionCategories.forEach(category => {
+        let categoryScore = data?.categories[category].score
+
+        categoriesScoresObject[category] = categoryScore
+    })
+
+    accessibilityScoresJSON[data.requestedUrl] = categoriesScoresObject
 
     const newAccessibilityJSON = JSON.stringify(accessibilityScoresJSON)
 
@@ -25,10 +43,14 @@ const execResult = (err = null, out, outerr = null) => {
 }
 
 const testURL = (urlToCheck, options = {}) => {
-    const commandToRun = `lighthouse ${urlToCheck} --only-categories=${lighthouseCategories(options?.categories ?? "accessibility")} --output json`
+    const isOptionsCategories = (!Object.isEmpty(options.categories) ?? false) ? `--only-categories=${lighthouseCategories(options?.categories ?? "accessibility")}` : ""
+    const commandToRun = `lighthouse ${urlToCheck} ${isOptionsCategories} --output json`
+
     console.log(`running command: ${commandToRun}`)
+
     series([
-        () => exec(commandToRun, execResult)
+        () => exec(commandToRun, execResult),
+        // () => exec("lighthouse https://emptywork.my.id --output json >> dump", (err, stdout, stderr) => console.log(stdout))
     ])
 }
 
@@ -37,11 +59,4 @@ const lighthouseCategories = (categories = []) => {
     return categories.join(',')
 }
 
-urlList.forEach(url => testURL(url,
-    {
-        "categories": [
-            "accessibility",
-            "performance"
-        ]
-    })
-)
+urlList.forEach(url => testURL(url, options))
