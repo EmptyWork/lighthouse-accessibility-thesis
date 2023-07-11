95587ac67ad55bcfb0b68c3532e867a321539bb0
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-07-11 10:22:19 +0900

    Update `commandHandlers.js` more return options

--- src/lib/commandHandlers.js
+++ src/lib/commandHandlers.js
@@ -2,7 +2,9 @@ import { getCategoriesFlags } from "./categoriesHandlers.js"
 
 const makeCommandFromURL = (url, options) => {
     const isOptionsCategories = getCategoriesFlags(options?.categories)
-    return `lighthouse ${url} ${isOptionsCategories}--output json --disable-full-page-screenshot`
+    const currentFlags = `${isOptionsCategories}--output json --disable-full-page-screenshot --chrome-flags="--no-sandbox --headless --disable-gpu"`
+    const commandToRun = `lighthouse ${url} ${currentFlags}`
+    return { commandToRun, currentFlags }
 }
 
 export { makeCommandFromURL }
