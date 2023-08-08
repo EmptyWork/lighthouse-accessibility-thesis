c8413be847000f5022e9786b5520654b2c6c3176
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2023-08-08 19:14:45 +0900

    Update `categoriesHandlers.class.js` renaming and add legacy support

--- src/lib/categoriesHandlers.class.js
+++ src/lib/categoriesHandlers.class.js
@@ -1,22 +1,31 @@
 import Utility from './utilities.class.js'
 
 class Categories {
-    getCategoriesFlags = array => {
-        return (!Array.isEmpty(array) ?? false) ? `--only-categories=${lighthouseCategories(array ?? "accessibility")} ` : ""
+    // getCategoriesFlags = array => {
+    getFlags = array => {
+        return (!Utility.isEmpty(array) ?? false) ? `--only-categories=${lighthouseCategories(array ?? "accessibility")} ` : ""
     }
 
-    getCurrentCategories = categories => {
+    // getCurrentCategories = categories => {
+    getCurrent = categories => {
         return categories ?? ["accessibility", "pwa", "best-practices", "performance", "seo"]
     }
 
-    lighthouseCategories = (categories = []) => {
+    // lighthouseCategories = (categories = []) => {
+    lighthouse = (categories = []) => {
         return (typeof categories == "string") ? categories : categories.join(',')
     }
 
-    getCategoriesFromCategoriesFlags = array => {
+    // getCategoriesFromCategoriesFlags = array => {
+    getFromFlags = array => {
         return (!Utility.isEmpty(array)) ? array : undefined
     }
 }
 
-export const { getCategoriesFlags, lighthouseCategories, getCurrentCategories, getCategoriesFromCategoriesFlags } = new Categories()
-export default Categories
+export const {
+    getFlags: getCategoriesFlags, // deprecated: only use this for non-class implementation 
+    lighthouse: lighthouseCategories, // deprecated: only use this for non-class implementation 
+    getCurrent: getCurrentCategories, // deprecated: only use this for non-class implementation 
+    getFromFlags: getCategoriesFromCategoriesFlags // deprecated: only use this for non-class implementation 
+} = new Categories()
+export default new Categories()
